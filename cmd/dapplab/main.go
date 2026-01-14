6e4e705f4e29a027e09a91ddbdfea4d4828156a2
Teta <tetref@users.noreply.github.com>
2026-01-14 20:31:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/dapplab/main.go
@@ -0,0 +1,23 @@
+// cmd/dapplab/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "dapplab/internal/dapplab"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := dapplab.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
